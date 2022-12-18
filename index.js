fb9bf29e7a2ba6deb183e4f5c7c0be39dae074f0
Maria Gabi <99148193+Gabszeran@users.noreply.github.com>
2022-12-18 18:41:09 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let numero1 = 5;
+let numero2 = 20;
+
+let soma = numero1 + numero2;
+let subtracao = numero1 - numero2;
+let divisao = numero1 / numero2;
+let multiplicacao = numero1 * numero2;
+
+console.log(`${numero1}+${numero2}=${soma}`);
+console.log(`${numero1}-${numero2}=${subtracao}`);
+console.log(`${numero1}/${numero2}=${divisao}`);
+console.log(`${numero1}*${numero2}=${multiplicacao}`);
